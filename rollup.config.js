710b825580e1bb92bd2a58804f2fec3006adef30
cdellacqua <carlo.dellacqua97@gmail.com>
2021-08-05 23:27:16 +0200

    bundle with rollup; add cjs and esm outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,70 +1,22 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
 
-function serve() {
-	let server;
+const external = [
+	...Object.keys(pkg.devDependencies || {}),
+	...Object.keys(pkg.peerDependencies || {}),
+	...Object.keys(pkg.dependencies || {})
+];
 
-	function toExit() {
-		if (server) server.kill(0);
+export default [
+	{
+		input: 'src/index.ts',
+		external,
+		plugins: [
+			typescript()
+		],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
 	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true,
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		},
-	};
-}
-
-export default {
-	input: 'src/app.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: true,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: (css) => {
-				css.write('bundle.css');
-			},
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte'],
-		}),
-		commonjs({
-			include: ['node_modules/**', 'build/**',]
-		}),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		livereload('public'),
-	],
-	watch: {
-		clearScreen: false,
-	},
-};
+];
